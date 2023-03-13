888757ee5e642045aba3b9aba87db09138459eec
VHDuc <ghostgreatful@gmail.com>
2023-03-13 15:39:25 +0700

    gui1

--- app/src/main/java/com/example/androidbtl/MainActivity.java
+++ app/src/main/java/com/example/androidbtl/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    //hiep hoi ga
+    //hiep hoi g
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
