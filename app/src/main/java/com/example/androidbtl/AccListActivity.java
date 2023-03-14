3968a7df0a53e86fba1e7c94c6ae7cee90f45346
VHDuc <ghostgreatful@gmail.com>
2023-03-14 11:36:00 +0700

    4 more Layout

---
+++ app/src/main/java/com/example/androidbtl/AccListActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidbtl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acc_list);
+    }
+}
